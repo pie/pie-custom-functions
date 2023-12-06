c47084d2dd314a783f24e7e274d680e56dd4264d
Joe Worthington <joe@pie.co.de>
2023-12-06 15:01:28 +0000

    Check version on plugin load, if version in database don't match version in plugin run plugin

--- pie-custom-functions.php
+++ pie-custom-functions.php
@@ -40,22 +40,28 @@ register_activation_hook(__FILE__ , __NAMESPACE__ . '\pie_custom_functions_init'
 
 function pie_custom_functions_init(){
 
-    $mu_plugin_content = file_get_contents(plugin_dir_path(__FILE__) . 'pie_mu_custom_functions.php');
+    update_option('pie_custom_functions_version', get_plugin_data(__FILE__)['Version']);
+
+    $local_mu_plugin_file = plugin_dir_path(__FILE__) . 'pie_mu_custom_functions.php';
 
     // Set the path for the MU plugin file
 
     if(defined('WPMU_PLUGIN_DIR')){
-        $mu_plugin_path = WPMU_PLUGIN_DIR . '/pie_mu_custom_functions.php';
+        $mu_plugin_destination_file = WPMU_PLUGIN_DIR . '/pie_mu_custom_functions.php';
     }
 
 
-    // Check if the MU plugin file already exists
-    if (!file_exists($mu_plugin_path)) {
-        // Create the MU plugin file
-        file_put_contents($mu_plugin_path, $mu_plugin_content);
+    rename($local_mu_plugin_file, $mu_plugin_destination_file);
+}
+
+add_action('plugins_loaded', __NAMESPACE__ . '\update_check')
+function update_check(){
+    $current_version = get_option('pie_custom_functions_version');
+    $new_version = get_plugin_data(__FILE__)['Version'];
+
+    if(version_compare($current_version, $new_version, '<')){
+        pie_custom_functions_init();
     }
-    
-    unlink(plugin_dir_path(__FILE__) . 'pie_mu_custom_functions.php');
 }
 
 
