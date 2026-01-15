1b4a59f5e880b957d9638f00e037df987623b304
Neil Miller <neil@pie.co.de>
2026-01-15 18:31:27 +0000

    Coding standards & PHP Doc

--- pie-custom-functions-mu.php
+++ pie-custom-functions-mu.php
@@ -39,36 +39,45 @@ if ( version_compare( PHP_VERSION, '8.0', '<' ) ) {
 
 include_once plugin_dir_path( __FILE__ ) . 'pie/redirections.php';
 
-// Add custom user role for Pie Admin
-function add_pie_admin_role()
+/**
+ * Add custom user role for Pie Admin
+ * 
+ * Creates a new 'pie_admin' role with administrator capabilities.
+ * Removes any existing role with the same name first.
+ * 
+ * @since 1.0.0
+ * @return void
+ */
+function add_pie_admin_role(): void
 {
-    remove_role('pie_admin');
-    add_role('pie_admin', 'Pie Admin', get_role('administrator')->capabilities);
+    remove_role( 'pie_admin' );
+    add_role( 'pie_admin', 'Pie Admin', get_role( 'administrator' )->capabilities );
 }
 // Hook the function into the 'init' action with a lower priority
-add_action('init', __NAMESPACE__ . '\add_pie_admin_role', 10);
+add_action( 'init', __NAMESPACE__ . '\add_pie_admin_role', 10 );
 
 
 /**
  * Add the 'Pie Admin' role to any existing user that has an email address ending in '@pie.co.de'
- * This function runs when the plugin is activated
  * 
+ * This function runs during plugin initialization and searches for all users
+ * with @pie.co.de email addresses, then assigns them the pie_admin role.
+ * 
+ * @since 1.0.0
  * @return void
  */
+function add_pie_admin_role_to_existing_users(): void
+{
+    $users = get_users( array(
+        'search' => '*@pie.co.de',
+        'search_columns' => array( 'user_email' ),
+    ) );
 
- function add_pie_admin_role_to_existing_users()
- {
- 
-     $users = get_users(array(
-         'search' => '*@pie.co.de',
-         'search_columns' => array('user_email'),
-     ));
- 
-     foreach ($users as $user) {
-         assign_pie_admin_to_user($user);
-     }
- }
- add_action('init', __NAMESPACE__ . '\add_pie_admin_role_to_existing_users', 10);
+    foreach ( $users as $user ) {
+        assign_pie_admin_to_user( $user );
+    }
+}
+add_action( 'init', __NAMESPACE__ . '\add_pie_admin_role_to_existing_users', 10 );
 
 /**
  * Add a meta box to the user profile page to allow the user to select a custom role
@@ -76,253 +85,323 @@ add_action('init', __NAMESPACE__ . '\add_pie_admin_role', 10);
  * 
  * @return void
  */
-if (is_multisite()) {
+if ( is_multisite() ) {
 
     /**
      * Add a custom meta box to the user profile page
      * 
+     * Registers action hooks to display the custom role selection
+     * meta box on both user profile editing pages.
+     * 
+     * @since 1.0.0
      * @return void
      */
-    add_action('admin_init', __NAMESPACE__ . '\custom_user_profile_meta_box');
+    add_action( 'admin_init', __NAMESPACE__ . '\custom_user_profile_meta_box' );
 
-    function custom_user_profile_meta_box()
+    function custom_user_profile_meta_box(): void
     {
-        add_action('show_user_profile', __NAMESPACE__ . '\custom_user_role_meta_box_callback');
-        add_action('edit_user_profile', __NAMESPACE__ . '\custom_user_role_meta_box_callback');
+        add_action( 'show_user_profile', __NAMESPACE__ . '\custom_user_role_meta_box_callback' );
+        add_action( 'edit_user_profile', __NAMESPACE__ . '\custom_user_role_meta_box_callback' );
     }
 
     /**
      * Display the custom meta box on the user profile page
+     * 
+     * Renders a dropdown selection for assigning custom user roles.
+     * Currently supports the 'pie_admin' role selection.
      *
-     * @param [type] $user
+     * @since 1.0.0
+     * @param \WP_User $user The user object being edited
      * @return void
      */
-    function custom_user_role_meta_box_callback($user)
+    function custom_user_role_meta_box_callback( \WP_User $user ): void
     {
-        $selected_role = get_user_meta($user->ID, 'custom_user_role', true);
+        $selected_role = get_user_meta( $user->ID, 'custom_user_role', true );
 
         echo '<h3>Custom User Role</h3>';
         echo '<table class="form-table"><tr>';
         echo '<th><label for="custom_user_role">Select Custom User Role:</label></th>';
         echo '<td><select name="custom_user_role" id="custom_user_role">';
 
-        $selected = selected($selected_role, 'pie_admin', false);
+        $selected = selected( $selected_role, 'pie_admin', false );
         echo "<option value='' $selected>Select Role</option>";
         echo "<option value='pie_admin' $selected>Pie Admin</option>";
 
         echo '</select></td></tr></table>';
     }
 
-    // Save the selected custom role when the user profile is updated
-    add_action('personal_options_update', __NAMESPACE__ . '\custom_save_user_role');
-    add_action('edit_user_profile_update', __NAMESPACE__ . '\custom_save_user_role');
+    /**
+     * Save the selected custom role when the user profile is updated
+     * 
+     * Validates user permissions and sanitizes the role selection before
+     * saving it to user meta data.
+     * 
+     * @since 1.0.0
+     * @param int $user_id The ID of the user being updated
+     * @return void
+     */
+    add_action( 'personal_options_update', __NAMESPACE__ . '\custom_save_user_role' );
+    add_action( 'edit_user_profile_update', __NAMESPACE__ . '\custom_save_user_role' );
 
-    function custom_save_user_role($user_id)
+    function custom_save_user_role( int $user_id ): void
     {
-        if (current_user_can('edit_user', $user_id)) {
-            $selected_role = sanitize_key($_POST['custom_user_role']);
-            update_user_meta($user_id, 'custom_user_role', $selected_role);
+        if ( current_user_can( 'edit_user', $user_id ) && isset( $_POST['custom_user_role'] ) ) {
+            $selected_role = sanitize_key( $_POST['custom_user_role'] );
+            update_user_meta( $user_id, 'custom_user_role', $selected_role );
         }
     }
 
-    // Set the custom role after the user has been saved
-    add_action('profile_update', __NAMESPACE__ . '\custom_set_user_role',);
+    /**
+     * Set the custom role after the user has been saved
+     * 
+     * Retrieves the selected custom role from user meta and assigns
+     * it to the user if a valid role was selected.
+     * 
+     * @since 1.0.0
+     * @param int $user_id The ID of the user being updated
+     * @return void
+     */
+    add_action( 'profile_update', __NAMESPACE__ . '\custom_set_user_role' );
 
-    function custom_set_user_role($user_id)
+    function custom_set_user_role( int $user_id ): void
     {
-        $selected_role = get_user_meta($user_id, 'custom_user_role', true);
+        $selected_role = get_user_meta( $user_id, 'custom_user_role', true );
 
-
-        if ($selected_role) {
-            $user = get_userdata($user_id);
-            $user->add_role($selected_role);
+        if ( $selected_role ) {
+            $user = get_userdata( $user_id );
+            if ( $user ) {
+                $user->add_role( $selected_role );
+            }
         }
-
-        $user = get_userdata($user_id);
     }
 }
 
 /**
  * Add the 'Pie Admin' role to any user that registers with an email address ending in '@pie.co.de'
  * 
- * @param int $user_id
+ * This function is automatically triggered when a new user registers.
+ * It checks their email domain and assigns the pie_admin role if appropriate.
+ * 
+ * @since 1.0.0
+ * @param int $user_id The ID of the newly registered user
  * @return void
  */
+add_action( 'user_register', __NAMESPACE__ . '\add_pie_admin_role_to_user_pie_email' );
 
-add_action('user_register', __NAMESPACE__ . '\add_pie_admin_role_to_user_pie_email');
-
-function add_pie_admin_role_to_user_pie_email($user_id)
+function add_pie_admin_role_to_user_pie_email( int $user_id ): void
 {
-    $user = get_userdata($user_id);
-    assign_pie_admin_to_user($user);
+    $user = get_userdata( $user_id );
+    if ( $user ) {
+        assign_pie_admin_to_user( $user );
+    }
 }
 
 
 
 /**
- * Get the current users email address
+ * Assign pie_admin role to user if they have a @pie.co.de email address
  * 
- * @param object $user
- * @return string $email
+ * Checks the user's email domain and adds the 'pie_admin' role
+ * if the domain matches 'pie.co.de'.
+ * 
+ * @since 1.0.0
+ * @param \WP_User $user The user object to check and potentially assign role to
+ * @return void
  */
-
-function assign_pie_admin_to_user($user)
+function assign_pie_admin_to_user( \WP_User $user ): void
 {
     $email = $user->user_email;
-    $email = explode('@', $email);
-    $email = $email[1];
-    if ($email == 'pie.co.de') {
-        $user->add_role('pie_admin');
+    $email_parts = explode( '@', $email );
+    
+    if ( 2 === count( $email_parts ) ) {
+        $domain = $email_parts[1];
+        if ( 'pie.co.de' === $domain ) {
+            $user->add_role( 'pie_admin' );
+        }
     }
 }
 
 /**
- * Remove the 'Brand Pro' and 'Pie Custom Functions' plugins from the plugins page for all users except Pie Admin
+ * Remove specific plugins from the plugins page for all users except Pie Admin
  * 
- * @param array $plugins
- * @return array $plugins
+ * Hides 'Ultimate Branding' and 'PIE Custom Functions' plugins from the plugins
+ * list for users who don't have the 'pie_admin' role.
+ * 
+ * @since 1.0.0
+ * @param array $plugins Array of all plugins
+ * @return array Modified array of plugins with certain plugins potentially removed
  */
+add_filter( 'all_plugins', __NAMESPACE__ . '\hide_plugins_on_plugins_page' );
 
-add_filter('all_plugins', __NAMESPACE__ . '\hide_plugins_on_plugins_page');
-function hide_plugins_on_plugins_page($plugins)
+function hide_plugins_on_plugins_page( array $plugins ): array
 {
     $current_user = wp_get_current_user();
 
-    if (!in_array('pie_admin', $current_user->roles)) {
-        if (isset($plugins['ultimate-branding/ultimate-branding.php'])) {
-            unset($plugins['ultimate-branding/ultimate-branding.php']);
+    if ( ! in_array( 'pie_admin', $current_user->roles, true ) ) {
+        if ( isset( $plugins['ultimate-branding/ultimate-branding.php'] ) ) {
+            unset( $plugins['ultimate-branding/ultimate-branding.php'] );
         }
-        if (isset($plugins['pie-custom-functions/pie-custom-functions.php'])) {
-            unset($plugins['pie-custom-functions/pie-custom-functions.php']);
+        if ( isset( $plugins['pie-custom-functions/pie-custom-functions.php'] ) ) {
+            unset( $plugins['pie-custom-functions/pie-custom-functions.php'] );
         }
     }
+    
     return $plugins;
 }
 
 /**
- * Remove the 'Ultimate Branding' tips post type and 'Ultimate Branding' menu item from the admin menu for all users except Pie Admin
+ * Remove Ultimate Branding menu items from admin sidebar for non-Pie Admin users
  * 
+ * Hides Ultimate Branding related menu items and tips post type from the admin
+ * sidebar for users who don't have the 'pie_admin' role.
+ * 
+ * @since 1.0.0
  * @return void
  */
-add_action('admin_menu', __NAMESPACE__ . '\hide_plugins_from_side_bar');
-add_action('network_admin_menu', __NAMESPACE__ . '\hide_plugins_from_side_bar');
-function hide_plugins_from_side_bar()
+add_action( 'admin_menu', __NAMESPACE__ . '\hide_plugins_from_side_bar' );
+add_action( 'network_admin_menu', __NAMESPACE__ . '\hide_plugins_from_side_bar' );
+
+function hide_plugins_from_side_bar(): void
 {
     $current_user = wp_get_current_user();
 
-    if (!in_array('pie_admin', $current_user->roles)) {
-        add_filter('branda_permissions_allowed_roles', '__return_empty_array');
-        remove_menu_page('edit.php?post_type=admin_panel_tip');
-        remove_menu_page('wpmudev-videos');
+    if ( ! in_array( 'pie_admin', $current_user->roles, true ) ) {
+        add_filter( 'branda_permissions_allowed_roles', '__return_empty_array' );
+        remove_menu_page( 'edit.php?post_type=admin_panel_tip' );
+        remove_menu_page( 'wpmudev-videos' );
     }
 }
 
 /**
- * Only allow access to the 'WPMU DEV' plugin item for Pie Admin
+ * Limit WPMU DEV plugin access to pie_admin users only
  * 
- * @return void
+ * Restricts WPMU DEV plugin functionality to users with the 'pie_admin' role
+ * by setting the WPMUDEV_LIMIT_TO_USER constant with pie admin user IDs.
+ * 
+ * @since 1.0.0
  */
-
-
-$users = get_users(array(
+$users = get_users( array(
     'role' => 'pie_admin'
-));
+) );
 
-$pie_admins = wp_list_pluck($users, 'ID');
+$pie_admins = wp_list_pluck( $users, 'ID' );
 
-
-if (!empty($pie_admins)) {
-    define('WPMUDEV_LIMIT_TO_USER', implode(',', $pie_admins));
+if ( ! empty( $pie_admins ) ) {
+    define( 'WPMUDEV_LIMIT_TO_USER', implode( ',', $pie_admins ) );
 }
 
 
 /**
- * Staging setup, check the url and if it contains the word staging then begin staging setup
- * Always set to staging as WPMU DEV has staging setup for all sites
+ * Staging site setup and configuration
  * 
- * @return void
+ * Performs staging site detection and applies appropriate configurations
+ * including spam filtering and domain mapping for multisite networks.
+ * 
+ * @since 1.0.0
  */
-
 
 // Before performing checks, ensure there is a live site url, if not, assume this is the live site
 set_duplicate_site_url_lock();
 
-if (is_staging_site()) {
+if ( is_staging_site() ) {
 
-    //  Marks all WPCF7 forms as not spam if on staging sie
-    add_filter('wpcf7_spam', '__return_false');
+    // Marks all WPCF7 forms as not spam if on staging site
+    add_filter( 'wpcf7_spam', '__return_false' );
 
-    //  Hooks into admin_init for code specific to the area on staging sites
-    add_action('admin_init', __NAMESPACE__ . '\staging_setup');
+    // Hooks into admin_init for code specific to the area on staging sites
+    add_action( 'admin_init', __NAMESPACE__ . '\staging_setup' );
     
 }
 
-function staging_setup(){
-   
-    if( is_multisite()) {
+/**
+ * Configure staging site specific settings
+ * 
+ * Handles multisite domain mapping updates for staging environments.
+ * Currently applies domain mapping fixes for staging.tempurl.host environments.
+ * 
+ * @since 1.0.0
+ * @return void
+ */
+function staging_setup(): void
+{
+    if ( is_multisite() ) {
         // Temporary fix to avoid issues with staging on multisite
-        if( strpos($_SERVER['HTTP_HOST'], 'staging.tempurl.host') !== false){
+        if ( false !== strpos( $_SERVER['HTTP_HOST'], 'staging.tempurl.host' ) ) {
             update_domain_mapping();
         }
     }
-
 }
 
 /**
  * Update the domain mapping for all sites on the multisite network
  * 
- * For example if the main site is https://pie.staging.tempurl.host and the staging site is https://.pie.co.de then the staging site will be updated to https://pie.co.de.pie.staging.tempurl.host
+ * Maps staging domains to production-style URLs for testing purposes.
+ * For example: pie.co.de becomes pie.co.de.pie.staging.tempurl.host
  * 
- * TO Do: Check on how get_site_url and other functions work with multisite.
- * 
- * To Do: Ensure that links in network admin are updated to the new domain
- * 
+ * @since 1.0.0
+ * @todo Check how get_site_url and other functions work with multisite
+ * @todo Ensure that links in network admin are updated to the new domain
  * @return void
  */
-
-
-function update_domain_mapping(){
+function update_domain_mapping(): void
+{
     $sites = get_sites();
-    $main_site_url = get_site_url(1);
+    $main_site_url = get_site_url( 1 );
 
-    $main_site_url = str_replace('http://', '', $main_site_url);
-    $main_site_url = str_replace('https://', '', $main_site_url);
-    foreach($sites as $site){
+    $main_site_url = str_replace( array( 'http://', 'https://' ), '', $main_site_url );
+    
+    foreach ( $sites as $site ) {
         $site_id = $site->blog_id;
         $site_url = $site->domain;
-        if('1' == $site_id || strpos($site_url, 'staging.tempurl.host') ){
+        
+        if ( '1' === $site_id || false !== strpos( $site_url, 'staging.tempurl.host' ) ) {
             continue;
         }
+        
         $site_domain = $site_url . '.' . $main_site_url;
         $site_url = 'https://' . $site_url . '.' . $main_site_url;
-        update_blog_details($site_id, array('domain' => $site_domain, 'path' => '/'));
+        
+        update_blog_details( $site_id, array(
+            'domain' => $site_domain,
+            'path' => '/'
+        ) );
 
-        update_blog_option($site_id, 'siteurl', $site_url);
-        update_blog_option($site_id, 'home', $site_url);
+        update_blog_option( $site_id, 'siteurl', $site_url );
+        update_blog_option( $site_id, 'home', $site_url );
     }
 }
 
 
 /**
+ * Set up a protected site URL lock for staging detection
  * 
- *  Compares known live site url and curent url to check if this is a staging site
+ * Creates a protected version of the site URL that won't be affected
+ * by search and replace operations during staging site creation.
  * 
+ * @since 1.0.0
+ * @return void
  */
-
- 
-function set_duplicate_site_url_lock() {
-
+function set_duplicate_site_url_lock(): void
+{
     // Add option does not overwrite options that are already set
     add_option( 'pcf_siteurl', get_duplicate_site_lock_key() );
-
 }
 
-function get_duplicate_site_lock_key() {
-
+/**
+ * Generate a protected site URL key for staging detection
+ * 
+ * Creates a URL with an embedded constant that won't be affected by
+ * search and replace operations during staging site deployment.
+ * 
+ * @since 1.0.0
+ * @return string The protected site URL with embedded constant
+ */
+function get_duplicate_site_lock_key(): string
+{
     // Grabs site url from current site
-    $site_url = get_site_url( 'current_wp_site' );
+    $site_url = get_site_url();
 
-    // Inserts constant into url to ensure no search and replace done to staging will affect it and returns the value
+    // Inserts constant into url to ensure no search and replace done to staging will affect it
     return substr_replace(
         $site_url,
         '_[pcf_site_url]_',
@@ -331,20 +410,24 @@ function get_duplicate_site_lock_key() {
     );
 }
 
-function is_staging_site() {
-
+/**
+ * Determine if current site is a staging environment
+ * 
+ * Compares the current site URL with the protected live site URL
+ * to determine if this is a staging site or the live production site.
+ * 
+ * @since 1.0.0
+ * @return bool True if this is a staging site, false if live site
+ */
+function is_staging_site(): bool
+{
     // Gets both current site url and known live site url
     $pcf_current_site_url = get_option( 'siteurl' );
-    $pcf_live_site_url    = get_option( 'pcf_siteurl' );
+    $pcf_live_site_url = get_option( 'pcf_siteurl' );
 
     // Remove constant from saved live site url to produce accurate live site url
-    $live_site_url = str_replace('_[pcf_site_url]_', '', $pcf_live_site_url);
+    $live_site_url = str_replace( '_[pcf_site_url]_', '', $pcf_live_site_url );
 
     // Compare strings of current site and known live site, returns bool
-    if ( $live_site_url === $pcf_current_site_url ) {
-        return false;
-    } else {
-        return true;
-    }
-
+    return $live_site_url !== $pcf_current_site_url;
 }
